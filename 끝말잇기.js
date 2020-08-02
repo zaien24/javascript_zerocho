b07fffa360e6a6694d3c7ef841698f8d5ee4421e
zaien24 <zaien24@gmail.com>
2020-08-02 23:53:08 +0900

    2
    엔터 입력
    
    폼 수정
    기본 동작 막기

--- 끝말잇기.js
+++ 끝말잇기.js
@@ -2,21 +2,27 @@ var 바디 = document.body;
 var 단어 = document.createElement('div');
 단어.textContent = '제로초';
 document.body.append(단어);
+var 폼 = document.createElement('form');
+document.body.append(폼);
 var 입력창 = document.createElement('input');
-document.body.append(입력창);
+폼.append(입력창);
 var 버튼 = document.createElement('button');
 버튼.textContent = '입력!';
-document.body.append(버튼);
+폼.append(버튼);
 var 결과창 = document.createElement('div');
 document.body.append(결과창);
 
-버튼.addEventListener('click', function() {
+폼.addEventListener('submit', function 콜백함수(이벤트) {
+    이벤트.preventDefault();
     if (단어.textContent[단어.textContent.length -1] === 입력창.value[0]) {
         결과창.textContent = '딩동댕'
         단어.textContent = 입력창.value;
         입력창.value = '';
+        입력창.focus();
     } else {
         결과창.textContent = '땡'
+        입력창.value = '';
+        입력창.focus();
     }
     
 });
